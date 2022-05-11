9e165998b82563f46a09e7069fa69a18e19d6d02
Mohit Kumar <mk4227525@gmail.com>
2022-05-11 23:44:08 +0530

    added env example

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
