2e0cdf7297a255ff6c1550dc3bdfa84d649a7d75
Mohit Kumar <mk4227525@gmail.com>
2022-05-13 11:46:41 +0530

    added production values

--- config/db.js
+++ config/db.js
@@ -10,4 +10,13 @@ module.exports = {
     dialect: "postgres",
     logging: console.log,
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: console.log,
+  },
 };
